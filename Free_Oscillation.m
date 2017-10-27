aa202e4db146403ad2ec098c288e293fdaa81ccb
frozendonuts <32994631+frozendonuts@users.noreply.github.com>
2017-10-27 15:42:46 -0400

    Add files via upload

--- Free_Oscillation.m
+++ Free_Oscillation.m
@@ -11,20 +11,22 @@ g = 9.81;
 
 % time domain
 step = 3.3e-4;
-t = linspace(0,0.3,0.3/step+1);
+tend = 5;
+t = linspace(0,tend,tend/step+1);
 
 u = zeros(1,length(t));
 u_d = zeros(1,length(t));
 u_dd = zeros(1,length(t));
-u0 = 0;
-u_d0 = 0;
-u_dd0 = g;
+
+u(1) = 0;
+u_d(1) = 0;
+u_dd(1) = g;
 
 force = zeros(1,length(t));
 
 
 % Newmark time step integration
-for i = 2:length(t)-1
+for i = 2:length(t)
    
     u(i) = u(i-1) + step*u_d(i-1) + 0.5*step^2*u_dd(i-1);
     
@@ -35,3 +37,13 @@ for i = 2:length(t)-1
     u_d(i) = u_d(i-1) + step*0.5*(u_dd(i-1)+u_dd(i));
     
 end
+
+plot(u*1000,force)
+xlabel('compression [mm]')
+ylabel('Elastomer Force [N]')
+axis([0 1.1 0 1200])
+
+figure, plot(u*1000,force)
+xlabel('compression [mm]')
+ylabel('Elastomer Force [N]')
+axis([0.65 0.67 480 500])
